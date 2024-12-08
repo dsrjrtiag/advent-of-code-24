3d472a450f2c0bad2063dccd6bd4fa7e7153be63
Doug Rogers <douglas.rogers@tiag.net>
2024-12-07 22:45:11 -0500

    day 6 part 2 brute force on original path

--- day6/part2_brute.py
+++ day6/part2_brute.py
@@ -25,34 +25,35 @@ next_direction_map = {Direction.UP: Direction.RIGHT,
 def run():
     input_lines = read_lines(__file__, input_file_name=input_file)
 
-    not_obstacles = []
+    obstacles = []
 
     for index, line in enumerate(input_lines):
-        find_direction = re.findall("[\^<>v]", line)
+        find_direction = re.findall("[<>v^]", line)
 
         for char_index, char in enumerate(line):
-            if char == '.':
-                not_obstacles.append((char_index, index))
+            if char == '#':
+                obstacles.append((char_index, index))
         if len(find_direction) > 0:
             direction = find_direction[0]
             coord = line.find(direction), index
             direction = direction_map[direction]
 
+    original_coords = original_path(input_lines=input_lines, direction=direction, start_coord=coord)
+    original_coords.remove(coord)
+
     overlaps = []
     args = []
-    for new_obstacle in not_obstacles:
-        args.append([input_lines, direction, coord, new_obstacle])
+    for new_obstacle in original_coords:
+        test_obstacles = obstacles.copy()
+        test_obstacles.append(new_obstacle)
+        args.append([input_lines, direction, coord, test_obstacles])
 
     pool = Pool()
     overlaps = list(pool.starmap(check_for_overlap, args))
-    # for new_obstacle in not_obstacles:
-    #     overlaps.append(check_for_overlap(input_lines, direction, coord, new_obstacle))
-
-    # distinct = set(coords)
 
     print(sum(overlaps))
 
-def check_for_overlap(input_lines, direction, coord, new_obstacle):
+def check_for_overlap(input_lines, direction, coord, obstacles):
     
     boundaries = [-1, len(input_lines)]
     coords = []
@@ -61,23 +62,45 @@ def check_for_overlap(input_lines, direction, coord, new_obstacle):
             return 0
 
         next_coord = coord[0] + direction.value[0], coord[1] + direction.value[1]
-
-        if next_coord[0] in boundaries or next_coord[1] in boundaries:
-            return 0
         
         if (coord, direction) in coords:
             return 1
         
         coords.append((coord, direction))
 
-        if input_lines[next_coord[1]][next_coord[0]] == '#' or next_coord == new_obstacle:
+        if next_coord in obstacles:
             direction = next_direction_map[direction]
 
         next_coord = coord[0] + direction.value[0], coord[1] + direction.value[1]
 
         coord = next_coord
 
+def original_path(input_lines, direction, start_coord):
+    boundaries = [-1, len(input_lines)]
+    coords = []
+    coord = start_coord
 
+    while True:
+        # if len(coords) == 39:
+        #     print("Debug")
+        if coord[0] in boundaries or coord[1] in boundaries:
+            break
+
+        coords.append(coord)
+
+        next_coord = coord[0] + direction.value[0], coord[1] + direction.value[1]
+
+        if next_coord[0] in boundaries or next_coord[1] in boundaries:
+            break
+
+        if input_lines[next_coord[1]][next_coord[0]] == '#':
+            direction = next_direction_map[direction]
+
+        next_coord = coord[0] + direction.value[0], coord[1] + direction.value[1]
+
+        coord = next_coord
+
+    return set(coords)
 
 def main():
     run()
