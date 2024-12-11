72aae31104a94ad6a1b901f48a4212ba4ffe4311
Douglas Rogers <douglas.s.rogers.ctr@navy.mil>
2024-12-11 11:24:45 -0500

    day 11 part 1 sample

--- day11/part1.py
+++ day11/part1.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass
+from typing import Self
 from utils.file import read_lines
 from utils.input_processing import int_values
 
@@ -6,12 +7,28 @@ input_file = "input.txt"
 
 class Value():
     initial_value: int
-    digits: int
+    current_value: int
     splits: int = 0
 
     def __init__(self, initial_value: int):
         self.initial_value = initial_value
-        self.digits  = len(str(initial_value))
+        self.current_value = initial_value
+
+    @property
+    def digits(self) -> int:
+        return len(str(self.current_value))
+
+    def blink(self) -> list[Self]:
+        if self.current_value == 0:
+            self.current_value = 1
+            return [self]
+        if self.digits % 2 == 0:
+            # even
+            first = int(str(self.current_value)[:int(self.digits / 2)])
+            second = int(str(self.current_value)[int(self.digits / 2):])
+            return [Value(first), Value(second)]
+        self.current_value = self.current_value * 2024
+        return [self]
 
 def run():
     inputs = read_lines(__file__, input_file)
@@ -25,10 +42,10 @@ def run():
     for blink in range(max_blinks):
         new_values = []
         for value in values:
-            new_values.append(value.split())
+            new_values.extend(value.blink())
         values = new_values
 
-    print([value.initial_value for value in values])
+    print(len([value.initial_value for value in values]))
     
 def main():
     run()
