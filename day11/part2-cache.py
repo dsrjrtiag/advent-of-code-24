1980050101dd9e95bbc04eecf52c5e371dd327f2
Doug Rogers <douglas.rogers@tiag.net>
2024-12-12 09:22:37 -0500

    day 11 part 2 caching solution complete

--- day11/part2-cache.py
+++ day11/part2-cache.py
@@ -6,62 +6,43 @@ from utils.input_processing import int_values
 
 input_file = "input.txt"
 
-class Value():
-    initial_value: int
-    current_value: int
-    blinks: int = 0
-
-    def __init__(self, initial_value: int):
-        self.initial_value = initial_value
-        self.current_value = initial_value
-
-    def __hash__(self):
-        return self.initial_value * self.current_value * self.blinks
-
-    @property
-    def digits(self) -> int:
-        return len(str(self.current_value))
-
-    def blink(self) -> list[Self]:
-        self.blinks += 1
-        if self.current_value == 0:
-            self.current_value = 1
-            return [self]
-        if self.digits % 2 == 0:
-            # even
-            first = int(str(self.current_value)[:int(self.digits / 2)])
-            second = int(str(self.current_value)[int(self.digits / 2):])
-            return [Value(first), Value(second)]
-        self.current_value = self.current_value * 2024
-        return [self]
-
 def run():
     inputs = read_lines(__file__, input_file)
 
     input_line = inputs[0]
 
-    values = [Value(initial_value = int(text.strip())) for text in input_line.split()]
+    values = [int(text.strip()) for text in input_line.split()]
 
-    max_blinks = 25
+    max_blinks = 75
 
-    # stones = 0
-    # for value in values:
-        # stones += run_blinks(values=tuple([value]), blinks_left = max_blinks)
     stones = run_blinks(values=tuple(values), blinks_left=max_blinks)
 
     print(stones)
 
 @cache
-def run_blinks(values: list[Value], blinks_left: int) -> int:
+def run_blinks(values: list[int], blinks_left: int) -> int:
     count = 0
+    if blinks_left == 0:
+        return len(values)
+
     for value in values:
-        if blinks_left == 0:
-            return len(values)
         new_values = []
-        new_values.extend(value.blink())
+        new_values.extend(blink(value))
         count += run_blinks(tuple(new_values), blinks_left - 1)
 
     return count
+
+def blink(value: int):
+    if value == 0:
+        return [1]
+    digits = len(str(value))
+    if digits % 2 == 0:
+        # even
+        first = int(str(value)[:int(digits / 2)])
+        second = int(str(value)[int(digits / 2):])
+        return [first, second]
+    value = value * 2024
+    return [value]
     
 def main():
     run()
